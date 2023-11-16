1abcd299b2d42dd180b4302ce95cd11911fc8f9b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-11-16 15:16:35 -0500

    Add method for getting genre details

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,5 @@
 const Genre = require("../models/genre");
+const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Genre.
@@ -10,7 +11,22 @@ exports.genre_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Genre.
 exports.genre_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NYI: Genre detail: ${req.params.id}`);
+  const [genre, booksInGenre] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (genre === null) {
+    const err = new Error("Genre not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("genre_detail", {
+    title: "Genre Detail",
+    genre: genre,
+    genre_books: booksInGenre,
+  });
 });
 
 // Display Genre create form on GET.
